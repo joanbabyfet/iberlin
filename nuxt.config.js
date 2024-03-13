7465384100ef13e57bca2e2ccb3da7554c5fdb2b
alan <crwu0206@gmail.com>
2024-03-13 09:41:08 +0800

    fix env

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: { // public 下的变量可以在客户端和服务端使用
       BASE_URL: '', //全局api域名
-      MAINTENANCE_MODE: false //是否维护中
+      MAINTENANCE_MODE: false, //是否维护中
+      NUXT_ENV: process.env.NUXT_PUBLIC_ENV,
     },
   },
   app: {
